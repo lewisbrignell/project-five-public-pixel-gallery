cb0243262ae4b747842d6f26401056ada595057f
Lewis <lewisbrignell@gmail.com>
2019-11-28 13:11:25 -0500

    added base styles, favicon, header content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  return (
-    <div className="App">
+// the main class of the Public Pixel Gallery App
+class PPGApp extends Component {
 
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>public pixel gallery</h1>
+          <button className="makePixelArt">make pixel art</button>
+        </header>
+        
+      </div>
+    );
+  } // render
+  
+} // PPGApp
 
-export default App;
+export default PPGApp;
