53441e973df5668a69437b59b329a7e145df29d0
Lewis <lewisbrignell@gmail.com>
2019-11-28 16:18:33 -0500

    most of overall styles done, media queries and css grid pending

--- src/App.js
+++ src/App.js
@@ -11,7 +11,43 @@ class PPGApp extends Component {
             <h1>public pixel gallery</h1>
             <button className="make-pixel-art">make pixel art</button>
         </header>
-        
+        <main>
+          <div className="wrapper">
+            <article>
+              <h2>title of piece</h2>
+              <div className="art-container"></div>
+              <h3>pen name of creator</h3>
+              <p>date and time</p>
+            </article>
+            <article>
+              <h2>title of piece</h2>
+              <div className="art-container"></div>
+              <h3>pen name of creator</h3>
+              <p>date and time</p>
+            </article>
+            <article>
+              <h2>title of piece</h2>
+              <div className="art-container"></div>
+              <h3>pen name of creator</h3>
+              <p>date and time</p>
+            </article>
+            <article>
+              <h2>title of piece</h2>
+              <div className="art-container"></div>
+              <h3>pen name of creator</h3>
+              <p>date and time</p>
+            </article>
+            <article>
+              <h2>title of piece</h2>
+              <div className="art-container"></div>
+              <h3>pen name of creator</h3>
+              <p>date and time</p>
+            </article>
+          </div> {/* /.wrapper */}
+        </main>
+        <footer>
+          <h4>copyright lewis brignell 2019</h4>
+        </footer>
       </div>
     );
   } // render
